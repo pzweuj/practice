f88378caee82750c7323dc8cd42c7ceb654e2386
Zw Pan <pzweuj@live.com>
2019-05-09 22:56:46 +0800

    Update ML100D001.py

--- python/ML100D/ML100D001.py
+++ python/ML100D/ML100D001.py
@@ -1,6 +1,10 @@
 # pzw
 # 20190508
 
+from sklearn.compose import ColumnTransformer, make_column_transformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 import numpy as np
 import pandas as pd
 
@@ -9,35 +13,36 @@ dataset = pd.read_csv("Data.csv")
 # print(dataset)
 X = dataset.iloc[ : , :-1].values
 Y = dataset.iloc[ : , 3].values
+print(X)
 
 # 处理丢失数据
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.NAN, strategy="mean", fill_value=None, verbose=0, copy=True)
 imputer = imputer.fit(X[ : , 1:3])
 X[ : , 1:3] = imputer.transform(X[ : , 1:3])
+print(X)
 
 # 解析分类数据
 from sklearn.preprocessing import LabelEncoder
 labelencoder_X = LabelEncoder()
 X[ : , 0] = labelencoder_X.fit_transform(X[ : , 0])
+print(X)
 
 # 创建虚拟变量
 from sklearn.preprocessing import OneHotEncoder
-onehotencoder = OneHotEncoder(categorical_features = [0])
+onehotencoder = OneHotEncoder(categories="auto")
 X = onehotencoder.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder()
-Y =  labelencoder_Y.fit_transform(Y)
+Y = labelencoder_Y.fit_transform(Y)
+print(X)
 
-# print(X)
-
-# # 拆分数据集为训练集合和测试集合
+# 拆分数据集为训练集合和测试集合
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
+X_train, X_test, Y_train, Y_test = train_test_split(X , Y , test_size=0.2, random_state=0)
 
-# # 特征量化
+# 特征量化
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
-
-print(X)
+print(X_test)
