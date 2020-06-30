fc519857b7d3ea687eaab4fb3db7ba47a8b499ce
Zw Pan <pzweuj@live.com>
2020-06-30 09:30:10 +0800

    Update AmpCap.py

--- python/WGS_stat/AmpCap.py
+++ python/WGS_stat/AmpCap.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 # pzw
-# 20200107
+# 20200220
 # AmpCap
 # amplicon capture analysis
 
@@ -41,7 +41,7 @@ def bwa(outputDir, sample, thread):
 	samtools = "samtools"
 	hg19 = "hg19.fa"
 	cmd = """
-		{bwa} mem -t {thread} {hg19} \\
+		{bwa} mem -R "@RG\\tID:{sample}\\tSM:{sample}\\tPL:illumina\\tPU:Amplicon" -t {thread} {hg19} \\
 			{outputDir}/cleandata/{sample}_1.fq.gz \\
 			{outputDir}/cleandata/{sample}_2.fq.gz \\
 			| {samtools} view -bSh - | {samtools} sort -@ {thread} - -o {outputDir}/bam/{sample}.bam
@@ -103,10 +103,8 @@ def bamAnalysis(outputDir, sample, bedFile):
 	outputFile.write("rawBases\t" + str(rawBases) + "\n")
 	outputFile.write("cleanReads\t" + str(cleanReads) + "\n")
 	outputFile.write("cleanBases\t" + str(cleanBases) + "\n")
-	outputFile.write("mappingReads\t" + mappingReads + "\n\n\n")
+	outputFile.write("mappingReads\t" + mappingReads + "\n")
 
-
-	outputFile.write("# " + "\t".join(["AmpID", "chrom", "start", "end", "length", "averageDepth", "coverage", "是否达到均一标准", "比例\n"]))
 
 	depthSum = 0
 	basesSum = 0
@@ -142,8 +140,13 @@ def bamAnalysis(outputDir, sample, bedFile):
 
 		outputLine.append([name.split("\n")[0], chrom, start, end, str(length), averageDepth, covPercent])
 
+	mappingAverageDepth = float(basesSum) / float(lengthSum)
+	outputFile.write("averageDepth\t" + ("%.2f" % mappingAverageDepth) + "\n")
+	outputFile.write("# " + "\t".join(["AmpID", "chrom", "start", "end", "length", "averageDepth", "coverage", "是否达到均一标准", "比例\n"]))
+
+
 	for i in outputLine:
-		if float(i[5]) >= (float(basesSum) / float(lengthSum) * 0.2):
+		if float(i[5]) >= (mappingAverageDepth * 0.2):
 			uniform = "yes"
 		else:
 			uniform = "no"
@@ -156,27 +159,79 @@ def bamAnalysis(outputDir, sample, bedFile):
 	outputFile.close()
 	bed.close()
 
-def main(inputDir, outputDir, sample, thread, adapter, trim, bedFile):
-	print("[质控中]")
-	fastp(inputDir, outputDir, sample, thread, adapter, trim)
-	print("[比对中]")
-	bwa(outputDir, sample, thread)
-	print("[提取信息中]")
-	bedtools(outputDir, sample)
-	print("[获得结果中]")
-	bamAnalysis(outputDir, sample, bedFile)
-	print("[完成]")
+def freebayes(outputDir, sample):
+	freebayes = "reebayes-v1.3.1"
+	gatk = "gatk"
+	hg19 = "hg19.fa"
+	samtools = "samtools"
+
+	cmd = """
+		{gatk} MarkDuplicates -I {outputDir}/bam/{sample}.bam \\
+			-O {outputDir}/bam/{sample}.markdups.bam \\
+			-M {outputDir}/bam/{sample}.dups.txt
+		{samtools} index {outputDir}/bam/{sample}.markdups.bam
+		rm {outputDir}/bam/{sample}.dups.txt
+		{freebayes} -f {hg19} {outputDir}/bam/{sample}.markdups.bam \\
+			-r chr7:55248900-55249200 > {outputDir}/covStat/{sample}.EGFRPL.vcf
+	""".format(samtools=samtools, gatk=gatk, outputDir=outputDir, sample=sample, freebayes=freebayes, hg19=hg19)
+	os.system(cmd)
+
+	egfr_file = open(outputDir + "/covStat/" + sample + ".EGFRPL.vcf", "r")
+	egfr_file2 = open(outputDir + "/covStat/" + sample + ".EGFR.txt", "w")
+	egfr_loca1_reads = "0"
+	egfr_loca2_reads = "0"
+	egfr_file2.write("EGFR_Location\tReads\n")
+	for line in egfr_file:
+		if "55249003\t.\tCAG\tCACGTAG" in line:
+			egfr_loca1_reads = line.split("\t")[9].split(":")[2].split(",")[1]
+			egfr_file2.write("chr7:55249003(±2) ACGT\t" + egfr_loca1_reads + "\n")
+		elif "55249146\t.\tTCAAC" in line:
+			egfr_loca2_reads = line.split("\t")[9].split(":")[2].split(",")[1]
+			egfr_file2.write("chr7:55249101 TGCA\t" + egfr_loca2_reads + "\n")
+		elif "55249145\t.\tCTCA" in line:
+			egfr_loca2_reads = line.split("\t")[9].split(":")[2].split(",")[1]
+			egfr_file2.write("chr7:55249101 TGCA\t" + egfr_loca2_reads + "\n")
+		elif "55249100\t.\tGAC\tGATGCAC" in line:
+			egfr_loca2_reads = line.split("\t")[9].split(":")[2].split(",")[1]
+			egfr_file2.write("chr7:55249101 TGCA\t" + egfr_loca2_reads + "\n")
+		else:
+			continue
+	egfr_file.close()
+	egfr_file2.close()
+
+
+def main(inputDir, outputDir, sample, thread, adapter, trim, bedFile, egfr, bamMode):
+	if not bamMode:
+		print("[质控中] " + sample)
+		fastp(inputDir, outputDir, sample, thread, adapter, trim)
+		print("[比对中] " + sample)
+		bwa(outputDir, sample, thread)
+		print("[提取信息中] " + sample)
+		bedtools(outputDir, sample)
+		print("[获得结果中] " + sample)
+		bamAnalysis(outputDir, sample, bedFile)
+		if egfr:
+			freebayes(outputDir, sample)
+		print("[完成] " + sample)
+	else:
+		print("[提取信息中] " + sample)
+		bedtools(outputDir, sample)
+		print("[获得结果中] " + sample)
+		bamAnalysis(outputDir, sample, bedFile)
+		if egfr:
+			freebayes(outputDir, sample)
+		print("[完成] " + sample)
 
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(
-		description="Amplicon Capture Analysis  PZW@Genephar",
+		description="Amplicon Capture Analysis  PZW",
 		prog="AmpCap.py",
 		usage="python3 AmpCap.py [-h] -s <sampleID> -i <rawdataDir> -o <outputDir> -b <bedFile>",
 		formatter_class=argparse.RawTextHelpFormatter
 	)
 	parser.add_argument("-v", "--version", action="version",
-		version="Version 0.2 20200107")
+		version="Version 0.3 20200630")
 	parser.add_argument("-i", "--rawdata", type=str,
 		help="Input the rawdata path")
 	parser.add_argument("-o", "--output", type=str,
@@ -190,10 +245,14 @@ if __name__ == "__main__":
 	parser.add_argument("-a", "--adapter", type=str,
 		help="adapter seq, option, default=auto detect", default="auto")
 	parser.add_argument("-trim", "--trim", type=str,
-		help="trim reads bases, option, default=0", default="0")	
+		help="trim reads bases, option, default=0", default="0")
+	parser.add_argument("-egfr", "--egfr", type=str,
+		help="EGFR Plasmid Test, '-egfr yes' to use", default="")
+	parser.add_argument("-bam", "--bamMode", type=bool,
+		help="input bam file，default'-bam False'", default=False)
 
 	if len(sys.argv[1:]) == 0:
 		parser.print_help()
 		parser.exit()
 	args = parser.parse_args()
-	main(inputDir=args.rawdata, outputDir=args.output, sample=args.sample, thread=args.thread, adapter=args.adapter, trim=args.trim, bedFile=args.bedfile)
+	main(inputDir=args.rawdata, outputDir=args.output, sample=args.sample, thread=args.thread, adapter=args.adapter, trim=args.trim, bedFile=args.bedfile, egfr=args.egfr, bamMode=args.bamMode)
