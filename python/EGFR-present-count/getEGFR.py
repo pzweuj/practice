692eb3e3b0a565869cf24f9b334a7188cce33576
Zw Pan <pzweuj@live.com>
2020-07-24 08:53:57 +0800

    Update getEGFR.py

--- python/EGFR-present-count/getEGFR.py
+++ python/EGFR-present-count/getEGFR.py
@@ -6,27 +6,27 @@ EGFRconclusion = open('existEGFR.txt', 'w')
 
 
 def existEGFR(inputfile, Hotspot=True):
-    import pandas as pd
-    df = pd.read_csv(inputfile, header=0, sep='\t')
-    if Hotspot == True:
-    	df2 = df.loc[(df['Allele Source'] == 'Hotspot') & (df['Allele Call'].str.contains('H'))]
-    else:
-    	df2 = df.loc[df['Allele Call'].str.contains('H')]
-    l = []
-    for i in df2['Gene ID']:
-        l.append(i)
-    if 'EGFR' in l:
-        conclue = inputfile + '-True'
-    else:
-        conclue = inputfile + '-False'
-    del df
-    del df2
-    return conclue
+	import pandas as pd
+	df = pd.read_csv(inputfile, header=0, sep='\t')
+	if Hotspot == True:
+		df2 = df.loc[(df['Allele Source'] == 'Hotspot') & (df['Allele Call'].str.contains('H'))]
+	else:
+		df2 = df.loc[df['Allele Call'].str.contains('H')]
+	l = []
+	for i in df2['Gene ID']:
+		l.append(i)
+	if 'EGFR' in l:
+		conclue = inputfile + '-True'
+	else:
+		conclue = inputfile + '-False'
+	del df
+	del df2
+	return conclue
 
 for name in filename:
-    openFilePath = './inputdata/' + name.split('\n')[0]
-    temp = existEGFR(openFilePath)
-    EGFRconclusion.write(temp + '\n')
+	openFilePath = './inputdata/' + name.split('\n')[0]
+	temp = existEGFR(openFilePath)
+	EGFRconclusion.write(temp + '\n')
 
 EGFRconclusion.close()
 filename.close()
